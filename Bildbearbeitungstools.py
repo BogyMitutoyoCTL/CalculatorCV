9993ceec110f79c2adec99e07832e57643fbeccf
Arthur Fibich <arthur.fibich@gmail.com>
2017-04-03 11:22:01 +0000

    Korrektur Grünfilter

--- Bildbearbeitungstools.py
+++ Bildbearbeitungstools.py
@@ -10,8 +10,8 @@ class Bildbearbeitungstools():
         hsv_bild = cv2.cvtColor(bgr_bild, cv2.COLOR_BGR2HSV)
         self.bildspeicher.add_bild(hsv_bild, self.bildspeicher.HSV)
 
-
     def glove_filter(self):
         lowerhsv = (self.datenbank.lower_h, self.datenbank.lower_s, self.datenbank.lower_v)
         upperhsv = (self.datenbank.upper_h, self.datenbank.upper_s, self.datenbank.upper_v)
-        self.grayimage = cv2.inRange(self.bildspeicher.get_bild(self.bildspeicher.HSV), lowerhsv, upperhsv)
+        hsv_bild = self.bildspeicher.get_bild(self.bildspeicher.HSV)
+        self.bildspeicher.add_bild(cv2.inRange(hsv_bild, lowerhsv, upperhsv), self.bildspeicher.GRAY)
