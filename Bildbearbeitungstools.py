08776d81867116af34d34039284dbc8b4adf5e46
Arthur Fibich <arthur.fibich@gmail.com>
2017-04-03 09:20:32 +0000

    Bild umwandeln von BGR zu HSV

---
+++ Bildbearbeitungstools.py
@@ -0,0 +1,10 @@
+import Bildspeicher
+import cv2
+
+class Bildbearbeitungstools():
+    def __init__(self):
+        self.bildspeicher = Bildspeicher()
+
+    def convert_brg2hsv(self):
+        self.bildspeicher.add_bild(cv2.cvtColor(self.bildspeicher.get_bild(0), cv2.COLOR_BGR2HSV), 1)
+
