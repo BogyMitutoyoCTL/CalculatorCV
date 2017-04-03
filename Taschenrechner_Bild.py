7fa6097d393a271957c0b20e13ed8b74f645a005
Ann-Sophie Gäckle <agaeckle@freenet.de>
2017-04-03 09:12:28 +0000

    Bilder/ Video anzeigen

---
+++ Taschenrechner_Bild.py
@@ -0,0 +1,21 @@
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
+
+camera = PiCamera()
+rawCapture = PiRGBArray(camera)
+camera.framerate = 4
+
+time.sleep(0.1)
+
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    image = frame.array
+
+    cv2.imshow("Frame", image)
+    key = cv2.waitKey(1) & 0xFF
+
+    rawCapture.truncate(0)
+
+    if key == ord("q"):
+		    break
