376253de7f0616ea4f7bec7bf159187b3fce766b
Arthur Fibich <arthur.fibich@gmail.com>
2017-04-03 13:01:53 +0000

    Zahlen auf bild anzeigen (erfolgreich im 4. Bild in Main getestet)

--- Main.py
+++ Main.py
@@ -3,6 +3,8 @@ import Bildbearbeitungstools
 import Bildspeicher
 import Datenbank
 import Taschenrechner_Fenster
+import Taschenrechner_Felder
+import Rechner
 
 
 
@@ -12,10 +14,13 @@ datenbank = Datenbank.Datenbank()
 camera = Taschenrechner_Bild.Picture(bildspeicher)
 tools = Bildbearbeitungstools.Bildbearbeitungstools(bildspeicher, datenbank)
 fenster = Taschenrechner_Fenster.Window(datenbank)
+rechner = Rechner.Rechner()
+felder = Taschenrechner_Felder.Feld(rechner, bildspeicher)
 
 
 
 fenster.create_window("Bilder")
+fenster.create_Trackbar("Bilder")
 camera.save_picture()
 bildspeicher.bild_anzeigen(False, "Bilder", bildspeicher.BGR, fenster)
 fenster.wait_key()
@@ -24,4 +29,7 @@ bildspeicher.bild_anzeigen(False, "Bilder", bildspeicher.HSV, fenster)
 fenster.wait_key()
 tools.glove_filter()
 bildspeicher.bild_anzeigen(False, "Bilder", bildspeicher.GRAY, fenster)
+fenster.wait_key()
+felder.rechenterm_anzeigen(3, 2, "+", bildspeicher.GRAY)
+bildspeicher.bild_anzeigen(True, "Bilder", bildspeicher.GRAY, fenster)
 fenster.wait_key()
