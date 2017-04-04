55362a8a6c575c60e68d236a3eca320f99e3cc0e
Arthur Fibich <arthur.fibich@gmail.com>
2017-04-04 09:15:12 +0000

    Feld Korrektur

--- Klasse_Feld.py
+++ Klasse_Feld.py
@@ -8,6 +8,37 @@ class Feld:
         self.bildspeicher = bildspeicher
         self.color = (255, 255, 255)
 
-    def draw_feld(self):
-        bild_mit_feld = cv2.rectangle(self.bildspeicher.get_bild(self.bildspeicher.BGR), self.punkt1, self.punkt2,self.color)
-        self.bildspeicher.add_bild_mit_felder(bild_mit_feld, self.bildspeicher.BGR)
+    def draw_feld(self, index):
+        bild_mit_feld = cv2.rectangle(self.bildspeicher.get_bild(index), self.punkt1, self.punkt2,self.color)
+        self.bildspeicher.add_bild_mit_felder(bild_mit_feld, index)
+
+    def set_punkt_1_x(self, wert):
+        self.punkt1 = (wert, self.punkt1[0])
+
+    def set_punkt_2_x(self, wert):
+        self.punkt2 = (wert, self.punkt2[0])
+
+    def set_punkt_1_y(self, wert):
+        self.punkt1 = (self.punkt1[0], wert)
+
+    def set_punkt_2_y(self, wert):
+        self.punkt2 = (self.punkt2[0], wert)
+
+    def set_color(self, color):
+        self.color = color
+
+    def get_punkt_1_x(self):
+        return self.punkt1[0]
+
+    def get_punkt_1_y(self):
+        return self.punkt1[1]
+
+    def get_punkt_2_x(self):
+        return self.punkt2[0]
+
+    def get_punkt_2_y(self):
+        return self.punkt2[1]
+
+    def get_color(self):
+        return self.color
+
