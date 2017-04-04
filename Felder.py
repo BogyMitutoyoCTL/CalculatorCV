99b9cc0b40f53ac5d3bacd0adb5e328e38e2137f
Paul Haney <PaulHaney@t-online.de>
2017-04-04 11:38:43 +0000

    Besprechung

--- Felder.py
+++ Felder.py
@@ -1,61 +1,67 @@
 import cv2
 
-Haupt_x = 1280                     #wie bekomme ich die Information aus dem Bild?
-Haupt_y = 720
-
-
-
 class Felder:
-    def __init__(self, input):
+    def __init__(self, filename):
 
-        self.picture = input
+        self.picture = cv2.imread(filename)
+        self.x = len(self.picture[0])
+        self.y = len(self.picture)
+        self.MitutoyoFarbe = (54, 120, 244)
 
-    def addieren(self, x, y):
+
+        self.windowName = None
+        cv2.namedWindow(self.windowName)
+
+    def addieren(self):
+
+        x = self.x
+        y = self.y
+
+        cv2.rectangle(self.picture, (x // 10, y // 10), (x // 10 * 4, y // 10 * 4), self.MitutoyoFarbe, 5)
+        cv2.putText(self.picture, "+", (x // 10 * 2, y // 10 * 2), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 0), 2)
+
+    def subtrahieren(self):
+
+        x = self.x
+        y = self.y
+
+        cv2.rectangle(self.picture, (x // 10 * 6, y // 10), (x // 10 * 9, y // 10 * 4), self.MitutoyoFarbe, 5)
+        cv2.putText(self.picture, "-", (x // 10 * 7, y // 10 * 2), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 0), 2)
+
+    def multiplizieren(self):
+
+        x = self.x
+        y = self.y
+
+        cv2.rectangle(self.picture, (x // 10, y // 10 * 6), (x // 10 * 4, y // 10 * 9), self.MitutoyoFarbe, 5)
+        cv2.putText(self.picture, "*", (x // 10 * 2, y // 10 * 7), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 0), 2)
+
+    def dividieren(self,):
+
+        x = self.x
+        y = self.y
 
         if y < x:
             r = y // 8
         else:
             r = x // 8
 
-        self.picture = cv2.circle(self.picture, (x // 6, y // 6), r, (255, 69,0))
-        self.picture = cv2.putText(self.picture, "+", (), cv2.FONT_HERSHEY_SIMPLEX, 11, (0, 0, 0))
+        cv2.rectangle(self.picture, (x // 10 * 6, y // 10 * 6), ( x // 10 * 9, y // 10 * 9), self.MitutoyoFarbe, 5)
+        cv2.putText(self.picture, "/", (x // 10 * 7, y // 10 * 7), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 0), 2)
 
-    def subtrahieren(self, x, y):
+    def show_image(self):
 
-        if y < x:
-            r = y // 8
-        else:
-            r = x // 8
-
-        self.picture = cv2.circle(self.picture, (x // 6 * 5, y // 6), r, (255, 69, 0))
-        self.picture = cv2.putText(self.picture, "-", (), cv2.FONT_HERSHEY_SIMPLEX, 11, (0, 0, 0))
-
-    def multiplizieren(self, x, y):
-
-        if y < x:
-            r = y // 8
-        else:
-            r = x // 8
-
-        self.picture = cv2.circle(self.picture, (x // 6, y // 6 * 5), r, (255, 69, 0))
-        self.picture = cv2.putText(self.picture, "*", (), cv2.FONT_HERSHEY_SIMPLEX, 11, (0, 0, 0))
-
-    def dividieren(self, x, y):
-
-        if y < x:
-            r = y // 8
-        else:
-            r = x // 8
-
-        self.picture = cv2.circle(self.picture, (x // 6 * 5, y // 6 * 5), r, (255, 69, 0))
-        self.picture = cv2.putText(self.picture, "/", (), cv2.FONT_HERSHEY_SIMPLEX, 11, (0, 0, 0))
+        cv2.imshow(self.windowName, self.picture)
 
     def waitKey(self):
 
         cv2.waitKey(0) & 0xFF
 
+
 test = Felder("white.png")
-test.addieren(Haupt_x, Haupt_y)
-test.subtrahieren(Haupt_x, Haupt_y)
-test.multiplizieren(Haupt_x, Haupt_y)
-test.dividieren(Haupt_x, Haupt_y)
+test.addieren()
+test.subtrahieren()
+test.multiplizieren()
+test.dividieren()
+test.show_image()
+test.waitKey()
