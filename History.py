d0f7cb323e741c8580f44d3d0eb3c1ecdb686fcc
Ann-Sophie Gäckle <agaeckle@freenet.de>
2017-04-06 09:23:48 +0000

    confirm_newest_finger

--- History.py
+++ History.py
@@ -1,18 +1,12 @@
-class History:
-    def __init__(self, framerate):
+import datetime
 
+class History:
+    def __init__(self):
         self.Handlist = []
-        self.framerate = framerate
-        self.i = 0
-        while self.i <= 2*framerate:
-            self.Handlist.append([None, None, None, None, None])
-            self.i += 1
 
     def add_information(self, position_of_hand_x, position_of_hand_y, time, number_of_fingers, operator):
 
-        self.Handlist[0:0] = [[position_of_hand_x, position_of_hand_y, time, number_of_fingers, operator]]
-        del self.Handlist[2*self.framerate+1]
-        #print(self.Handlist)
+        self.Handlist.insert(0,[position_of_hand_x, position_of_hand_y, time, number_of_fingers, operator])
 
     def get_position_of_hand_x(self, index):
         return self.Handlist[index][0]
@@ -30,21 +24,30 @@ class History:
         return self.Handlist[index][4]
 
     def confirm_fingers(self):
-        if self.get_number_of_fingers(2*self.framerate - 1) == self.get_number_of_fingers(0):
-            return self.get_number_of_fingers(0)
-        else:
-            return None
+        return self.confirm_newest_finger()
+
+    def confirm_newest_finger(self):
+        self.get_number_of_fingers(0)
+        time_now = self.get_time(0)
+        time_old = time_now
+        i = 1
+        while self.get_number_of_fingers(i) == self.get_number_of_fingers(0):
+            time_old = self.get_time(i)
+            i += 1
+        time_difference = time_now - time_old
+        return time_difference > datetime.timedelta(0, 2, 0, 0, 0, 0, 0)
 
 
-test = History(3)
-test.add_information(61, 5, 1, 8, None)
-test.add_information(54, 6, 2, 8, "+")
-test.add_information(1, 7, 3, 3, "-")
-test.add_information(61, 5, 1, 8, None)
-test.add_information(54, 6, 2, 8, "+")
-test.add_information(61, 5, 1, 8, None)
+test = History()
+test.add_information(61, 5, datetime.datetime(2017, 4, 6, 12, 45, 10), 6, None)
+test.add_information(54, 6, datetime.datetime(2017, 4, 6, 12, 45, 11), 6, "+")
+test.add_information(1, 7, datetime.datetime(2017, 4, 6, 12, 45, 12), 6, "-")
+test.add_information(61, 5, datetime.datetime(2017, 4, 6, 12, 45, 13), 5, None)
+test.add_information(54, 6, datetime.datetime(2017, 4, 6, 12, 45, 14), 8, "+")
+test.add_information(61, 5, datetime.datetime(2017, 4, 6, 12, 45, 19), 5, None)
+test.add_information(61, 5, datetime.datetime(2017, 4, 6, 12, 45, 20), 8, None)
 
 print(test.Handlist)
 print(test.confirm_fingers())
-if __name__ == "__main__":
-    print("Please run Main.")
+#if __name__ == "__main__":
+    #print("Please run Main.")
