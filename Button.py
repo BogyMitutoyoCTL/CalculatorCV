2752f71abb3b574426234ab8c7a5c3ec648f0ab6
Paul Haney <PaulHaney@t-online.de>
2017-04-06 09:55:11 +0000

    Refactor ButtonGenerator, Button

--- Button.py
+++ Button.py
@@ -1,36 +1,36 @@
 import cv2
-from PictureStorage import PictureStorage
-
 
 class Button:
     def __init__(self, top_x: int, top_y: int, bottom_x: int, bottom_y: int, text: str, scale):
         self.top_left = (top_x, top_y)
         self.bottom_right = (bottom_x, bottom_y)
-        self.color = (54, 120, 244)
-        self.thickness = 5
-        self.text = text
         self.top_x = top_x
         self.top_y = top_y
         self.bottom_x = bottom_x
         self.bottom_y = bottom_y
+        self.total_x = self.top_x + self.bottom_x
+        self.total_y = self.top_y + self.bottom_y
+        self.text = text
+        self.color = (54, 120, 244)
+        self.text_shape = cv2.FONT_HERSHEY_SIMPLEX
         self.scale = scale
+        self.thickness_rect = 5
+        self.thickness_text = 2
+        self.division = 2
 
 
     def draw_field(self, picture):
         picture = picture.copy()
-        picture = cv2.rectangle(picture, self.top_left, self.bottom_right, self.color, self.thickness)
+        picture = cv2.rectangle(picture, self.top_left, self.bottom_right, self.color, self.thickness_rect)
 
-        x = (self.top_x + self.bottom_x) // 2
-        y = (self.top_y + self.bottom_y) // 2
-
-        text_size = cv2.getTextSize(self.text, cv2.FONT_HERSHEY_SIMPLEX, self.scale, 2)
+        text_size = cv2.getTextSize(self.text, self.text_shape, self.scale, self.thickness_text)
         width = text_size[0][0]
         hight = text_size[0][1]
 
-        x = x - width // 2
-        y = y + hight // 2
+        x = self.total_x // self.division - width // self.division
+        y = self.total_y // self.division + hight // self.division
 
-        picture = cv2.putText(picture, self.text, (x, y), cv2.FONT_HERSHEY_SIMPLEX, self.scale, self.color, 2)
+        picture = cv2.putText(picture, self.text, (x, y), self.text_shape, self.scale, self.color, self.thickness_text)
 
         return picture
 
@@ -46,8 +46,5 @@ class Button:
     def get_bottom_y(self):
         return self.bottom_right[1]
 
-    def field_recognizion(self, top_x, bottom_x, top_y, bottom_y, handx, handy):
-        return top_x <= handx <= bottom_x and top_y <= handy <= bottom_y
-
-
-
+    def field_recognizion(self, hand_x, hand_y):
+        return self.top_x <= hand_x <= self.bottom_x and self.top_y <= hand_y <= self.bottom_y
--- ButtonGenerator.py
+++ ButtonGenerator.py
@@ -1,4 +1,3 @@
-import cv2
 from PictureStorage import PictureStorage
 from Button import Button
 
@@ -15,65 +14,62 @@ class ButtonGenerator:
         self.text_scale_del = 1
 
     def addieren(self):
-        x = self.width
-        y = self.hight
-
-        top_x = self.from_top_left(x)
-        top_y = self.from_top_left(y)
-        bottom_x = self.calculate_bottom(top_x, x)
-        bottom_y = self.calculate_bottom(top_y, y)
+        top_x = self.from_left()
+        top_y = self.from_top()
+        bottom_x = self.calculate_bottom(top_x, self.width)
+        bottom_y = self.calculate_bottom(top_y, self.hight)
         text = "+"
 
         return Button(top_x, top_y, bottom_x, bottom_y, text, self.text_scale)
 
     def subtrahieren(self):
-
-        x = self.width
-        y = self.hight
-
-        top_x = self.from_bottom_right(x)
-        top_y = self.from_top_left(y)
-        bottom_x = self.calculate_bottom(top_x, x)
-        bottom_y = self.calculate_bottom(top_y, y)
+        top_x = self.from_right()
+        top_y = self.from_top()
+        bottom_x = self.calculate_bottom(top_x, self.width)
+        bottom_y = self.calculate_bottom(top_y, self.hight)
         text = "-"
 
         return Button(top_x, top_y, bottom_x, bottom_y, text, self.text_scale)
 
     def multiplizieren(self):
-
-        x = self.width
-        y = self.hight
-
-
-        top_x = self.from_top_left(x)
-        top_y = self.from_bottom_right(y)
-        bottom_x = self.calculate_bottom(top_x, x)
-        bottom_y = self.calculate_bottom(top_y, y)
+        top_x = self.from_left()
+        top_y = self.from_bottom()
+        bottom_x = self.calculate_bottom(top_x, self.width)
+        bottom_y = self.calculate_bottom(top_y, self.hight)
         text = "*"
 
         return Button(top_x, top_y, bottom_x, bottom_y, text, self.text_scale)
 
-    def from_top_left(self, x):
-        return x // self.section_count * self.margin
-
-    def delete(self,):
-        x = self.width
-        y = self.hight
-
-        top_x = self.from_bottom_right(x)
-        top_y = self.from_bottom_right(y)
-        bottom_x = self.calculate_bottom(top_x, x)
-        bottom_y = self.calculate_bottom(top_y, y)
+    def delete(self):
+        top_x = self.from_right()
+        top_y = self.from_bottom()
+        bottom_x = self.calculate_bottom(top_x, self.width)
+        bottom_y = self.calculate_bottom(top_y, self.hight)
         text = "delete"
-
 
         return Button(top_x, top_y, bottom_x, bottom_y, text, self.text_scale_del)
 
     def draw_all_buttons(self):
         return [self.addieren(), self.subtrahieren(), self.multiplizieren(), self.delete()]
 
+    def from_top_left(self, x):
+        return x // self.section_count * self.margin
+
+    def from_top(self):
+        return self.from_top_left(self.hight)
+
+    def from_left(self):
+        return self.from_top_left(self.width)
+
     def from_bottom_right(self, x):
         return x // self.section_count * (self.section_count - self.button_size - self.margin)
+
+    def from_bottom(self):
+        return self.from_bottom_right(self.hight)
+
+    def from_right(self):
+        return self.from_bottom_right(self.width)
+
 
     def calculate_bottom(self, top_x, x):
         return top_x + self.button_size * x // self.section_count
