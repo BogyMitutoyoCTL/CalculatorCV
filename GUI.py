efcb7452604d093b4ced066c00978b9cdcb59e6c
Ann-SophieG <agaeckle@freenet.de>
2017-04-07 06:09:39 +0000

    Merge branch 'master' of /home/pi/PycharmProjects/calculatorCV/CalculatorCV with conflicts.

--- GUI.py
+++ GUI.py
@@ -9,61 +9,39 @@ class GUI:
         self.calculator = calculator
         self.picture_storage = picture_storage
         self.settings = settings
-        self.section_count = 15
-        self.default_button_hight = 2.5
 
-    def paint_term(self, number1, operator=None, number2=None):
-        image = self.picture_storage.get_picture(self.picture_storage.GUI_BGR).copy()
-        hight = len(image)
-        width = len(image[0])
+    def paint_term(self, number1=None, operator=None, number2=None, delete=None):
+        image = self.picture_storage.get_picture(self.picture_storage.ORIGINAL_FROM_CAMERA_BGR).copy()
 
-        if number2 is None and operator is None:
+        if number1 is not None and operator is None and number2 is None and delete is not None:
+            number1 = None
+            image_with_text= image
+            self.picture_storage.add_picture(image_with_text, self.picture_storage.ORIGINAL_WITH_FELD)
 
-            x = width / 2
-            y = hight / self.section_count * self.default_button_hight
-
+        if number1 is not None and operator is None and number2 is None and delete is None:
             string = str(number1)
-            text_size = cv2.getTextSize(string, cv2.FONT_HERSHEY_SIMPLEX, 1, 2)
-            text_x = text_size[0][0]
-            text_y = text_size[0][1]
+            image_with_text = cv2.putText(image, string, (0, 100), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 255, 255), 3)
+            self.picture_storage.add_picture(image_with_text, self.picture_storage.ORIGINAL_WITH_FELD)
 
-            x = x - text_x / 2
-            y = y + text_y / 2
+        if number1 is not None and operator is not None and number2 is None and delete is not None:
+            operator = None
+            string = str(number1)
+            image_with_text = cv2.putText(image, string, (0, 100), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 255, 255), 3)
+            self.picture_storage.add_picture(image_with_text, self.picture_storage.ORIGINAL_WITH_FELD)
 
-            image_with_text = cv2.putText(image, string, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
-            self.picture_storage.add_picture(image_with_text, self.picture_storage.GUI_BGR)
-
-        if number2 is None and operator is not None:
-
-            x = width / 2
-            y = hight / self.section_count * self.default_button_hight
-
+        if number1 is not None and operator is not None and number2 is None and delete is None:
             string = str(number1) + " " + operator
-            text_size = cv2.getTextSize(string, cv2.FONT_HERSHEY_SIMPLEX, 1, 2)
-            text_x = text_size[0][0]
-            text_y = text_size[0][1]
+            image_with_text = cv2.putText(image, string, (0, 100), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 255, 255), 3)
+            self.picture_storage.add_picture(image_with_text, self.picture_storage.ORIGINAL_WITH_FELD)
 
-            x = x - text_x / 2
-            y = y + text_y / 2
+        if number1 is not None and operator is not None and number2 is not None and delete is not None:
+            number2 = None
+            string = str(number1) + " " + operator
+            image_with_text = cv2.putText(image, string, (0, 100), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 255, 255), 3)
+            self.picture_storage.add_picture(image_with_text, self.picture_storage.ORIGINAL_WITH_FELD)
 
-            image_with_text = cv2.putText(image, string, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255),2)
-            self.picture_storage.add_picture(image_with_text, self.picture_storage.GUI_BGR)
-
-        if number2 is not None and operator is not None:
+        if number1 is not None and operator is not None and number2 is not None and delete is None:
             result = self.calculator.calculate(number1, number2, operator)
-
-            x = width / 2
-            y = hight / self.section_count * self.default_button_hight
-
-            string = str(number1) + " " + operator + " " + str(number2) + " " + "=" + " " + str(result)
-
-            text_size = cv2.getTextSize(string, cv2.FONT_HERSHEY_SIMPLEX, 1, 2)
-            text_x = text_size[0][0]
-            text_y = text_size[0][1]
-
-            x = x - text_x / 2
-            y = y + text_y / 2
-
-            image_with_text = cv2.putText(image, string, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255),
-                                          2)
-            self.picture_storage.add_picture(image_with_text, self.picture_storage.GUI_BGR)
+            string = str(number1) + " " + operator + " " + str(number2) + " " + "=" + "  " + str(result)
+            image_with_text = cv2.putText(image, string, (0, 100), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 255, 255), 3)
+            self.picture_storage.add_picture(image_with_text, self.picture_storage.ORIGINAL_WITH_FELD)
